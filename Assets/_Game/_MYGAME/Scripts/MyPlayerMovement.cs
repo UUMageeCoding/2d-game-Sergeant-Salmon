5f24d9bc58ea3d63129f379579229405d65fc78c
Sergeant-Salmon <Goody-L@ulster.ac.uk>
2025-10-28 15:45:14 +0000

    yuh
    las completed: animated rocks

--- Assets/_Game/_MYGAME/Scripts/MyPlayerMovement.cs
+++ Assets/_Game/_MYGAME/Scripts/MyPlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class MyPlayerMovement : MonoBehaviour
@@ -7,16 +8,30 @@ public class MyPlayerMovement : MonoBehaviour
     public float speed = 5;
     public Rigidbody2D rb;
     public Animator myanim;
+    public int facingDirection = 1;
 
-//FixedUpdate is called 50x per frame
+    //FixedUpdate is called 50x per frame
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+           horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
         myanim.SetFloat("Horizontal", Mathf.Abs(horizontal));
         myanim.SetFloat("Vertical", Mathf.Abs(vertical));
 
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+    
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
