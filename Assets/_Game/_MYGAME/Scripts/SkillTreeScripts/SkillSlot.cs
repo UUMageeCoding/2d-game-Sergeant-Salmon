e805adf57423c81afdbb789988b9c715117bb040
Sergeant-Salmon <Goody-L@ulster.ac.uk>
2025-11-25 15:41:33 +0000

    #swagmoney #weup

---
+++ Assets/_Game/_MYGAME/Scripts/SkillTreeScripts/SkillSlot.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillSlot : MonoBehaviour
+{
+
+    public SkillSO skillSO;
+    public int currentLevel;
+    public bool isUnlocked;
+    public Image skillIcon;
+    public TMP_Text skillLevelText;
+
+
+
+
+    private void OnValidate()
+    {
+        if (skillSO != null && skillLevelText != null)
+        {
+            UpdateUI();
+        }
+    }
+    
+
+    private void UpdateUI()
+    {
+        skillIcon.sprite = skillSO.skillIcon;
+        if (isUnlocked)
+        {
+            skillLevelText.text = currentLevel.ToString() + "/" + skillSO.maxLevel.ToString();
+            skillIcon.color = Color.white;
+        }
+        else
+        {
+            skillLevelText.text = "Locked";
+            skillIcon.color = Color.gray;
+        }
+    }
+}
